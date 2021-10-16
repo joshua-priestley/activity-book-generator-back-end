07f4b9a71d9142697ff6c9c41ccf9bd48fda6541
Daniel Kaye <djk18@ic.ac.uk>
2021-10-16 12:40:43 +0100

    Adds a test route

--- activitygen/root.py
+++ activitygen/root.py
@@ -1,7 +1,16 @@
+import re
 from flask import Blueprint, jsonify
 
 bp = Blueprint("root", __name__)
 
 @bp.route("/")
 def root():
+  return jsonify("Activity Book Generator back-end is running")
+
+# @bp.route("/pdf/")
+# def root():
+#   return {"pdf": "Pdf goes here"}
+
+@bp.route("/pdf/")
+def pdf():
   return jsonify("Activity Book Generator back-end is running")
