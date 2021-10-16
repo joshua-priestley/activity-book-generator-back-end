7399815829366df6d7a2fb911efc78c4a9bc481c
Daniel Kaye <djk18@ic.ac.uk>
2021-10-16 16:26:05 +0100

    Generates a test pdf inline

--- activitygen/root.py
+++ activitygen/root.py
@@ -1,5 +1,8 @@
 import re
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, make_response
+from flask.templating import render_template
+from werkzeug.utils import send_file
+import pdfkit
 
 bp = Blueprint("root", __name__)
 
@@ -7,10 +10,12 @@ bp = Blueprint("root", __name__)
 def root():
   return jsonify("Activity Book Generator back-end is running")
 
-# @bp.route("/pdf/")
-# def root():
-#   return {"pdf": "Pdf goes here"}
 
 @bp.route("/pdf/")
 def pdf():
-  return jsonify("Activity Book Generator back-end is running")
+  pdf = pdfkit.from_string("Test", False)
+
+  response = make_response(pdf)
+  response.headers['Content-Type'] = 'application/pdf'
+  response.headers['Content-Disposition'] = 'inline; filename=output.pdf'
+  return response
