a26e79916559484e3d9d846d9f9da3ee9d13d877
Arran O'Sullivan <alo19@ic.ac.uk>
2021-10-18 10:44:51 +0100

    Add difficulty, implement difficulty support for anagrams

--- activitygen/activities.py
+++ activitygen/activities.py
@@ -1,18 +1,29 @@
 import re
+from enum import Enum
 from flask import Blueprint, jsonify, request
-from random import Random
+from functools import total_ordering
+from random import getrandbits, Random
 from typing import List
 
 NOT_LOWER_ALPHA = re.compile("([^a-z]+)")
+
+@total_ordering
+class Difficulty(Enum):
+  EASY = 0
+  MEDIUM = 1
+  HARD = 2
+  def __lt__(self, other):
+    return self.value < other.value
 
 bp = Blueprint("activities", __name__, url_prefix="/activities")
 
 @bp.route("/anagrams")
 def anagrams():
   words = request.args.getlist("words")
-  return jsonify(generate_anagrams(words))
+  difficulty = Difficulty[request.args.get("difficulty", Difficulty.HARD).upper()]
+  return jsonify(generate_anagrams(words, difficulty))
 
-def generate_anagrams(words: List[str], seed=None) -> List[str]:
+def generate_anagrams(words: List[str], difficulty: Difficulty, seed=None) -> List[str]:
   anagrams: List[str] = []
 
   for word in words:
@@ -22,8 +33,40 @@ def generate_anagrams(words: List[str], seed=None) -> List[str]:
       if NOT_LOWER_ALPHA.fullmatch(split_word):
         continue
       chars = list(split_word)
-      Random(seed).shuffle(chars)
-      split_words[i] = "".join(chars)
+
+      if difficulty == Difficulty.EASY and len(chars) > 3:
+        # For easy difficulty (and len > 3): fix first and last characters
+        # We exclude them from shuffle and add them back at the end
+        chars = chars[1:-1]
+        recover_fixed = True
+      else:
+        recover_fixed = False
+
+      if difficulty <= Difficulty.MEDIUM and len(chars) > 3:
+        # For easy and medium difficulty (and len > 3): group pairs of characters
+
+        # There are two ways to group pairs (e.g., "he ll o" or "h el lo")
+        # Randomly pick between them with 50-50 probability
+        prefix = []
+        if bool(getrandbits(1)):
+          prefix.append(chars.pop(0))
+
+        grouped_chars = [c1 + c2 for c1, c2 in zip(chars[::2], chars[1::2])]
+        if len(chars) % 2 == 1:
+          grouped_chars.append(chars[-1])
+        chars = prefix + grouped_chars
+
+      # Shuffle until different
+      shuffled = chars
+      if len(chars) > 1:
+        while shuffled == chars:
+          shuffled = Random(seed).sample(chars, len(chars))
+      
+      if recover_fixed:
+        shuffled.insert(0, split_word[0])
+        shuffled.append(split_word[-1])
+
+      split_words[i] = "".join(shuffled)
 
     anagrams.append("".join(split_words))
   
--- tests/test_activities.py
+++ tests/test_activities.py
@@ -1,21 +1,25 @@
-from activitygen.activities import generate_anagrams
+import pytest
+from activitygen.activities import Difficulty, generate_anagrams
 from collections import Counter
 
-def test_anagram_gen_gives_rearrangement():
+@pytest.mark.parametrize("difficulty", (Difficulty.EASY, Difficulty.MEDIUM, Difficulty.HARD))
+def test_anagram_gen_gives_rearrangement(difficulty):
   """Anagrams should use exactly the same characters as the original strings"""
-  sample_words = ["cow", "reindeer", "skeleton", "princess", "christmas tree"]
+  sample_words = ["cow", "reindeer", "skeleton", "princess", "christmas tree", "a", "tv"]
   assert all(is_rearrangement(word, anagram)
-             for word, anagram in zip(sample_words, generate_anagrams(sample_words)))
+             for word, anagram in zip(sample_words, generate_anagrams(sample_words, difficulty)))
 
-def test_anagram_gen_makes_lowercase():
+@pytest.mark.parametrize("difficulty", (Difficulty.EASY, Difficulty.MEDIUM, Difficulty.HARD))
+def test_anagram_gen_makes_lowercase(difficulty):
   """Uppercase letters should be made lowercase"""
   sample_words = ["London Eye", "LiON"]
-  assert all(anagram.islower() for anagram in generate_anagrams(sample_words))
+  assert all(anagram.islower() for anagram in generate_anagrams(sample_words, difficulty))
 
-def test_anagram_gen_preserves_words():
+@pytest.mark.parametrize("difficulty", (Difficulty.EASY, Difficulty.MEDIUM, Difficulty.HARD))
+def test_anagram_gen_preserves_words(difficulty):
   """Letters from different words in the same string should not be mixed"""
   xmas_word, jacko_word = "christmas tree", "jack-o'-lantern"
-  xmas_anagram, jacko_anagram = generate_anagrams([xmas_word, jacko_word])
+  xmas_anagram, jacko_anagram = generate_anagrams([xmas_word, jacko_word], difficulty)
 
   assert is_rearrangement(xmas_word[:9], xmas_anagram[:9])
   assert xmas_anagram[9] == " "
