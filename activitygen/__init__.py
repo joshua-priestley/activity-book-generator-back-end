cc856534fc8c4ec9ca0ae72d7b6f1fa83bbd8678
Andrei-Florin Sburlan <as8319@ic.ac.uk>
2021-10-20 19:18:57 +0100

    Add redis based sessions

--- activitygen/__init__.py
+++ activitygen/__init__.py
@@ -1,16 +1,23 @@
-from flask import Flask
+from flask import Flask, session
+from flask_session import Session
+import redis
 
 def create_app(test_config=None):
   """Application factory"""
   app = Flask(__name__)
-
+ 
   # Use test config if supplied
   if test_config:
     app.config.from_mapping(test_config)
+
+  app.config["SESSION_TYPE"]  = "redis"
+  app.config["SESSION_REDIS"] = redis.from_url('redis://db:6379')
+
+  sess = Session()
+  sess.init_app(app)
 
   # Register blueprints
   from . import root
   app.register_blueprint(root.bp)
 
   return app
- 
