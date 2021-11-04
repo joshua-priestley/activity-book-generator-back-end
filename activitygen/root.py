670750610ca50394a3bc8568d98e857eac24446c
Andrei-Florin Sburlan <as8319@ic.ac.uk>
2021-11-04 11:57:03 +0000

    Remove backend pdf generation

--- activitygen/root.py
+++ activitygen/root.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, jsonify, make_response, request, session
-import pdfkit
 import random
 #from werkzeug.wrappers import request
 
@@ -19,31 +18,31 @@ def root():
 def puzzles():
   return jsonify(["Word Search", "Anagrams"])
 
-@bp.route("/word-search")
-def word_search_endpoint():
-  words = ["squirrel", "wolf", "bear", "lion", "tiger", "tortoise"]
-  hidden_message = "We love animals"
-  (cells, hangman_words, _) = word_search.generate(words, hidden_message)
-  data = {
-    "cells": cells,
-    "hangman_words": hangman_words,
-    "words": words,
-  }
-  options = {
-    "encoding": "UTF-8",
-    "page-size": "A4",
-    "dpi": 400,
-    "disable-smart-shrinking": "",
-    "margin-top": "1in",
-    "margin-right": "1in",
-    "margin-bottom": "1in",
-    "margin-left": "1in"
-  }
-  pdf = pdfkit.from_string(word_search.generate_html(data), False, options=options, css="style/styles.css")
-  response = make_response(pdf)
-  response.headers['Content-Type'] = 'application/pdf'
-  response.headers['Content-Disposition'] = 'inline; filename=output.pdf'
-  return response
+#@bp.route("/word-search")
+#def word_search_endpoint():
+#  words = ["squirrel", "wolf", "bear", "lion", "tiger", "tortoise"]
+#  hidden_message = "We love animals"
+#  (cells, hangman_words, _) = word_search.generate(words, hidden_message)
+#  data = {
+#    "cells": cells,
+#    "hangman_words": hangman_words,
+#    "words": words,
+#  }
+#  options = {
+#    "encoding": "UTF-8",
+#    "page-size": "A4",
+#    "dpi": 400,
+#    "disable-smart-shrinking": "",
+#    "margin-top": "1in",
+#    "margin-right": "1in",
+#    "margin-bottom": "1in",
+#    "margin-left": "1in"
+#  }
+#  pdf = pdfkit.from_string(word_search.generate_html(data), False, options=options, css="style/styles.css")
+#  response = make_response(pdf)
+#  response.headers['Content-Type'] = 'application/pdf'
+#  response.headers['Content-Disposition'] = 'inline; filename=output.pdf'
+#  return response
 
 
 @bp.route("/themes", methods = ['GET', 'POST'])
@@ -84,94 +83,94 @@ html_gen_map = {
 # "anagrams": 3,
 # "wordsearch": 4
 # } 
-@bp.route("/generate", methods=["post"])
-def generate():
-  body = request.json
- 
-  userThemes = themes.copy()
+#@bp.route("/generate", methods=["post"])
+#def generate():
+#  body = request.json
+# 
+#  userThemes = themes.copy()
+#
+#  if "custom themes" in session:
+#    userThemes = { **userThemes, **session["custom themes"] }
+#
+#
+#  wordset = userThemes[body["theme"]]
+#  numWords = len(wordset)
+#
+#  json = []
+#  for _ in range(body["Anagrams"]):
+#    # select random words for anagram
+#    puzzleWords = random.sample(wordset, min(numWords, 10))
+#    json.append({"activity": "anagrams", "inputs": {
+#      "theme": body["theme"],
+#      "words": puzzleWords,
+#      "difficulty": anagrams.Difficulty.HARD
+#    }})
+#    
+#  for n in range(body["Word Search"]):
+#    l = [w for w in wordset if " " not in w]
+#    puzzleWords = random.sample(l, min(len(l), 6))
+#    json.append({"activity": "word-search", "inputs": {
+#      "hidden_message": puzzleWords.pop(),
+#      "words": puzzleWords
+#    }})
+#
+#  for n in range(body["Maze"]):
+#    json.append({
+#    "activity": "maze", 
+#    "inputs": {
+#      "grid_width": 15,
+#      "grid_height": 15
+#    }, 
+#    "html_data": {
+#      "title": "maze",
+#      "instructions": "Complete the maze"}
+#    })
+#  #   maze = Maze(15,15,0,0)
+#  #   maze.make_maze()
+#  #   maze.generate_svg()
+#
+#  
+#  for _ in range(body["Fill In the Blanks"]):
+#    puzzleWords = random.sample(wordset, min(numWords, 10))
+#    json.append({"activity": "fill-in-the-blanks", "inputs": {
+#      "theme": body["theme"],
+#      "words": puzzleWords,
+#      "difficulty": anagrams.Difficulty.HARD
+#    }})
+#  
+#  random.shuffle(json)
+#
+#  pdf_result = pdf(json)
+#  response = make_response(pdf_result)
+#  response.headers['Content-Type'] = 'application/pdf'
+#  response.headers['Content-Disposition'] = 'inline; filename=output.pdf'
+#  return response
 
-  if "custom themes" in session:
-    userThemes = { **userThemes, **session["custom themes"] }
-
-
-  wordset = userThemes[body["theme"]]
-  numWords = len(wordset)
-
-  json = []
-  for _ in range(body["Anagrams"]):
-    # select random words for anagram
-    puzzleWords = random.sample(wordset, min(numWords, 10))
-    json.append({"activity": "anagrams", "inputs": {
-      "theme": body["theme"],
-      "words": puzzleWords,
-      "difficulty": anagrams.Difficulty.HARD
-    }})
-    
-  for n in range(body["Word Search"]):
-    l = [w for w in wordset if " " not in w]
-    puzzleWords = random.sample(l, min(len(l), 6))
-    json.append({"activity": "word-search", "inputs": {
-      "hidden_message": puzzleWords.pop(),
-      "words": puzzleWords
-    }})
-
-  for n in range(body["Maze"]):
-    json.append({
-    "activity": "maze", 
-    "inputs": {
-      "grid_width": 15,
-      "grid_height": 15
-    }, 
-    "html_data": {
-      "title": "maze",
-      "instructions": "Complete the maze"}
-    })
-  #   maze = Maze(15,15,0,0)
-  #   maze.make_maze()
-  #   maze.generate_svg()
-
-  
-  for _ in range(body["Fill In the Blanks"]):
-    puzzleWords = random.sample(wordset, min(numWords, 10))
-    json.append({"activity": "fill-in-the-blanks", "inputs": {
-      "theme": body["theme"],
-      "words": puzzleWords,
-      "difficulty": anagrams.Difficulty.HARD
-    }})
-  
-  random.shuffle(json)
-
-  pdf_result = pdf(json)
-  response = make_response(pdf_result)
-  response.headers['Content-Type'] = 'application/pdf'
-  response.headers['Content-Disposition'] = 'inline; filename=output.pdf'
-  return response
-
-def pdf(activities):
-  html = []
-  for activity in activities:
-    # Generate activity internal representation
-    data = activity_map[activity["activity"]](**activity["inputs"])
-
-    data = process(data, activity)
-
-    # Generate HTML and append to output
-    if "html_data" in activity:
-      html.append(html_gen_map[activity["activity"]](data, activity["html_data"]))
-    else:  
-      html.append(html_gen_map[activity["activity"]](data))
-    
-    
-  return pdfkit.from_string("".join(html), False, css="style/styles.css", options={
-    "encoding": "UTF-8",
-    "page-size": "A4",
-    "dpi": 400,
-    "disable-smart-shrinking": "",
-    "margin-top": "1in",
-    "margin-right": "1in",
-    "margin-bottom": "1in",
-    "margin-left": "1in"
-  })
+#def pdf(activities):
+#  html = []
+#  for activity in activities:
+#    # Generate activity internal representation
+#    data = activity_map[activity["activity"]](**activity["inputs"])
+#
+#    data = process(data, activity)
+#
+#    # Generate HTML and append to output
+#    if "html_data" in activity:
+#      html.append(html_gen_map[activity["activity"]](data, activity["html_data"]))
+#    else:  
+#      html.append(html_gen_map[activity["activity"]](data))
+#    
+#    
+#  return pdfkit.from_string("".join(html), False, css="style/styles.css", options={
+#    "encoding": "UTF-8",
+#    "page-size": "A4",
+#    "dpi": 400,
+#    "disable-smart-shrinking": "",
+#    "margin-top": "1in",
+#    "margin-right": "1in",
+#    "margin-bottom": "1in",
+#    "margin-left": "1in"
+#  })
 
 def process(data, activity):
   if activity["activity"] == "word-search":
