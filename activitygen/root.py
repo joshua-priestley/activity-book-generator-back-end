608a85432f14f25a800f2c67ab26d1f999973c75
Andrei-Florin Sburlan <as8319@ic.ac.uk>
2021-11-04 13:14:17 +0000

    Fixes

--- activitygen/root.py
+++ activitygen/root.py
@@ -22,7 +22,7 @@ def puzzles():
 @bp.route("/whoami")
 @check_token
 def whoami():
-  return jsonify({"user" : str(request.user.email) })
+  return jsonify({"user" : str(request.user["email"]) })
 
 #@bp.route("/word-search")
 #def word_search_endpoint():
