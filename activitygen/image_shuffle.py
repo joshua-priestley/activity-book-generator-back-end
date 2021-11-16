f0fead4b21fdf81270bb6d9f8cb22602b7d5462d
sj3818 <sj3818@ic.ac.uk>
2021-11-16 14:07:16 +0000

    Adds comments, refactors code, changes endpoints and adds TODOs for endpoint

--- activitygen/image_shuffle.py
+++ activitygen/image_shuffle.py
@@ -5,35 +5,53 @@ import math
 
 
 def image_shuffler(original_img_path, num_tiles):
-    # original_img = Image.open('christmas.jpg')
-
+    # Opens image from given file path
     try:
         original_img = Image.open(original_img_path)
     except IOError:
+        print("No image found at filepath")
+        original_img = Image.open('christmas.jpg')
         pass
 
     width, height = original_img.size
 
+    num_w_tiles = 1
+    num_h_tiles = 1
+
+    # If number of tiles is greater than 1, splits into tiles and concatenates shuffles tiles into another image
     if num_tiles > 1:
+        # Calculates the optimum splits for each side of the puzzle to keep tiles most square like
         (num_w_tiles, num_h_tiles) = calc_num_splits_on_each_side(width, height, num_tiles)
 
+        # Splits images into tiles
         tile_list, area_list = split_into_tiles(width, height, num_w_tiles, num_h_tiles, original_img)
 
+        # Shuffles tiles and combines them into one image
         shuffled_image = concatenate_tiles(tile_list, width, height, area_list)
 
+    # If number of tiles is 1 or less, then return original image
     else:
         shuffled_image = original_img
 
-    shuffled_image.save(original_img_path[0:-4] + "_shuffled.jpg")
+    shuffled_image_path = original_img_path[0:-4] + "_shuffled.jpg"
+
+    # Save shuffled image
+    shuffled_image.save(shuffled_image_path)
+
+    return shuffled_image_path, num_w_tiles, num_h_tiles
 
 
 def calc_num_splits_on_each_side(width, height, num_tiles):
 
+    # Get factor pairs of num_tiles
     factors = []
     for whole_number in range(1, int(math.sqrt(num_tiles)) + 1):
         if num_tiles % whole_number == 0:
             factors.append((whole_number, num_tiles // whole_number))
 
+    # For each factor pair, divides the longest side by the largest factor, and the shortest side by the smallest
+    # factor, calculates the difference between width and height splits and picks the best number of splits for each
+    # side to ensure tile width and height are most square-like
     smallest_diff = max(width, height)
     w_num_tiles = 0
     h_num_tiles = 0
@@ -56,12 +74,16 @@ def split_into_tiles(width, height, num_w_t, num_h_t, original_img):
     area_list = []
     tile_list = []
 
+    # Calculates width and height split points
     width_splits = [i * width / num_w_t for i in range(0, num_w_t + 1)]
     height_splits = [i * height / num_h_t for i in range(0, num_h_t + 1)]
 
+    # Creates a list of areas for the tiles using the width and height split points
     for i in range(0, num_w_t):
         for j in range(0, num_h_t):
             area_list.append((width_splits[i], height_splits[j], width_splits[i + 1], height_splits[j + 1]))
+
+    # Crops the image for each area and creates a list of tiles
     for i, area in enumerate(area_list):
         tile_list.append(original_img.crop(area))
 
@@ -101,16 +123,23 @@ bp = Blueprint("image_shuffle", __name__, url_prefix="/activities/image_shuffle"
 @bp.route("/state")
 def get_state():
     """Returns image tiles from from provided options"""
-    # difficulty = Difficulty.from_str(request.args.get("difficulty"), Difficulty.HARD)
-    image = request.files.get('image_file', "")
+    image = request.files.get('image_file', 'christmas.jpg')
     num_tiles = request.args.get("num_tiles", 9)
 
-    tiles = image_shuffler(image, num_tiles)
+    # TODO save image and give path to image shuffler function
+    image_path = 'christmas.jpg'
+
+    shuffled_image_path, w_num_tiles, h_num_tiles = image_shuffler(image_path, num_tiles)
 
     return {
         "description": [
             f"Recreate the original image by shuffling the tiles and draw the original image in the empty box or"
             "write the corresponding tile numbers"
         ],
-        "tiles": tiles,
+        # TODO how to pass back image?
+        "tiles": shuffled_image_path,
+        "w_num_tiles": w_num_tiles,
+        "h_num_tiles": h_num_tiles
     }
+
+    # TODO delete the image and shuffled image
