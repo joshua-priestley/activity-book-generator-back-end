70e79c5da84e716bd202cec447cbbd9b8f029fe1
sj3818 <sj3818@ic.ac.uk>
2021-11-16 12:38:36 +0000

    Adds split into tiles function

--- activitygen/image_shuffle.py
+++ activitygen/image_shuffle.py
@@ -11,31 +11,51 @@ def image_shuffler(original_img_path, num_tiles):
     width, height = original_img.size
 
     if num_tiles <= 1:
-        dst = original_img
+        shuffled_image = original_img
+
     if num_tiles == 2:
-        area_list = []
-        tile_list = []
         if width >= height:
-            area_list.append((0, 0, width / 2, height))
-            area_list.append((width/2, 0, width, height))
+            num_w_t = 2
+            num_h_t = 1
         else:
-            area_list.append((0, 0, width, height / 2))
-            area_list.append((0, height / 2, width, height))
+            num_w_t = 1
+            num_h_t = 2
 
-        for i, area in enumerate(area_list):
-            tile_list.append(original_img.crop(area))
+        tile_list = split_into_tiles(width, height, num_w_t, num_h_t, original_img)
 
-        if width >= height:
-            dst = Image.new('RGB', (tile_list[0].width + tile_list[1].width, tile_list[0].height))
-            dst.paste(tile_list[1], (0, 0))
-            dst.paste(tile_list[0], (tile_list[1].width, 0))
+        shuffled_image = concatenate_tiles(tile_list)
 
-    dst.save(original_img_path[0:-4] + "_shuffled.jpg")
+    shuffled_image.save(original_img_path[0:-4] + "_shuffled.jpg")
+
+
+def split_into_tiles(width, height, num_w_t, num_h_t, original_img):
+    area_list = []
+    tile_list = []
+
+    width_splits = [i * width / num_w_t for i in range(0, num_w_t + 1)]
+    height_splits = [i * height / num_h_t for i in range(0, num_h_t + 1)]
+
+    for i in range(0, num_w_t):
+        for j in range(0, num_h_t):
+            area_list.append((width_splits[i], height_splits[j], width_splits[i + 1], height_splits[j + 1]))
+
+    for i, area in enumerate(area_list):
+        tile_list.append(original_img.crop(area))
+
+    return tile_list
+
+
+def concatenate_tiles(tile_list, width, height):
+    if width >= height:
+        dst = Image.new('RGB', (tile_list[0].width + tile_list[1].width, tile_list[0].height))
+        dst.paste(tile_list[1], (0, 0))
+        dst.paste(tile_list[0], (tile_list[1].width, 0))
+    return dst
 
 
 if __name__ == "__main__":
     image_shuffler('christmas.jpg', 2)
-
+    # print(range(0, 2111, 2111 / 2))
 
 bp = Blueprint("image_shuffle", __name__, url_prefix="/activities/image_shuffle")
 
