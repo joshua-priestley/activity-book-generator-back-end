7fc6a8668226597f473faf426a9bdb3b0d3f62f4
sj3818 <sj3818@ic.ac.uk>
2021-11-16 13:46:37 +0000

    Adds calc number of splits on each side function to split the image into even tiles

--- activitygen/image_shuffle.py
+++ activitygen/image_shuffle.py
@@ -1,32 +1,55 @@
 from flask import Blueprint, request
 from PIL import Image
 import random
+import math
 
 
 def image_shuffler(original_img_path, num_tiles):
-    original_img = Image.open('christmas.jpg')
+    # original_img = Image.open('christmas.jpg')
+
     try:
         original_img = Image.open(original_img_path)
     except IOError:
         pass
+
     width, height = original_img.size
 
-    if num_tiles <= 1:
-        shuffled_image = original_img
+    if num_tiles > 1:
+        (num_w_tiles, num_h_tiles) = calc_num_splits_on_each_side(width, height, num_tiles)
 
-    if num_tiles == 2:
-        if width >= height:
-            num_w_t = 2
-            num_h_t = 1
-        else:
-            num_w_t = 1
-            num_h_t = 2
-
-        tile_list, area_list = split_into_tiles(width, height, num_w_t, num_h_t, original_img)
+        tile_list, area_list = split_into_tiles(width, height, num_w_tiles, num_h_tiles, original_img)
 
         shuffled_image = concatenate_tiles(tile_list, width, height, area_list)
 
+    else:
+        shuffled_image = original_img
+
     shuffled_image.save(original_img_path[0:-4] + "_shuffled.jpg")
+
+
+def calc_num_splits_on_each_side(width, height, num_tiles):
+
+    factors = []
+    for whole_number in range(1, int(math.sqrt(num_tiles)) + 1):
+        if num_tiles % whole_number == 0:
+            factors.append((whole_number, num_tiles // whole_number))
+
+    smallest_diff = max(width, height)
+    w_num_tiles = 0
+    h_num_tiles = 0
+    for i, factor_pair in enumerate(factors):
+        diff = (max(width, height) / max(factor_pair) - min(width, height) / min(factor_pair))
+        diff = abs(diff)
+        if diff < smallest_diff:
+            smallest_diff = diff
+            if width >= height:
+                w_num_tiles = max(factor_pair)
+                h_num_tiles = min(factor_pair)
+            else:
+                w_num_tiles = min(factor_pair)
+                h_num_tiles = max(factor_pair)
+
+    return w_num_tiles, h_num_tiles
 
 
 def split_into_tiles(width, height, num_w_t, num_h_t, original_img):
@@ -39,7 +62,6 @@ def split_into_tiles(width, height, num_w_t, num_h_t, original_img):
     for i in range(0, num_w_t):
         for j in range(0, num_h_t):
             area_list.append((width_splits[i], height_splits[j], width_splits[i + 1], height_splits[j + 1]))
-
     for i, area in enumerate(area_list):
         tile_list.append(original_img.crop(area))
 
@@ -70,8 +92,8 @@ def concatenate_tiles(tile_list, width, height, area_list):
 
 
 if __name__ == "__main__":
-    image_shuffler('christmas.jpg', 2)
-    # print(range(0, 2111, 2111 / 2))
+    image_shuffler('christmas.jpg', 3)
+
 
 bp = Blueprint("image_shuffle", __name__, url_prefix="/activities/image_shuffle")
 
