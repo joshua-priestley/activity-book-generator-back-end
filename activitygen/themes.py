ade9e223586b4bc39965c4358f38617dc7dc2d18
Tiberiu Bucur <tiberiu.bucur19@imperial.ac.uk>
2021-11-18 18:05:07 +0000

    Add Machine Learning library for theming

--- activitygen/themes.py
+++ activitygen/themes.py
@@ -1,8 +1,13 @@
 import random
 import re
 from typing import List
+import gensim.downloader
+from nltk import edit_distance
 
 ALPHABETIC = re.compile(r"[a-zA-Z]+")
+
+model = gensim.downloader.load('word2vec-google-news-300')
+# model = None
 
 themes = {
   "christmas" : ["santa", "christmas tree", "reindeer", "present", "elf", "snowman", "bauble", "stocking", "christmas pudding", "turkey", "angel", "jesus", "evergreen", "sleigh"],
@@ -11,12 +16,75 @@ themes = {
   "cities" : ["london", "new york", "chicago", "los angeles", "edinburgh", "hong kong", "tokyo", "singapore", "amsterdam", "berlin", "singapore", "sydney", "melbourne", "bangkok", "dubai", "milan", "toronto", "budapest", "shanghai"]
 }
 
+# This is to select how many words we get out of the model, in terms of the count variable
+# The number of words is gonna be TIMES_MORE_WORDS * count, out of which we pick count final words
+TIMES_MORE_WORDS = 5
+
+def filter_func(word: str, theme: str):
+  # We do not allow any other character except letters and underscore
+  if re.search('[^a-zA-z_]', word) != None:
+    return False
+  
+  # A reasonable similarity is at least a third of the letters are different
+  return edit_distance(word.lower(), theme.lower()) > len(theme) // 3
+
+def query_model(theme: str, count: int):
+  try:
+    # Get the top count most similar words to string
+    ans = list(map((lambda p: p[0]), model.most_similar(theme, topn=count)))
+    # And filter out the ones which are too similar
+    ans = list(filter((lambda w: filter_func(w, theme)), ans))
+    
+    # Calculate how many words we still have to get and divide them equally
+    n = len(ans)
+    diff = n - count
+    if diff > 0:
+      old = set(ans)
+      new = set()
+      for res in ans:
+        l = list(filter((lambda s: s not in old, new), query_model(res, diff)))
+        new.update(l)
+      ans += random.sample(new, diff)
+    
+    return ans 
+  except KeyError:
+    return []
+
+def try_formats(theme: str, no_results: int):
+  theme = add_underscore(theme)
+
+  # Formats that are the most likely to be contained in the model and return meaningful results
+  id = lambda s: s
+  lower = lambda s: s.lower()
+  title = lambda s: s.title()
+  formats = [id, lower, title]
+
+  for f in formats:
+    res = query_model(f(theme), no_results)
+    if res != []:
+      break
+
+  return list(map(remove_underscore, res))
+  
+def remove_underscore(string: str):
+  return string.replace('_', ' ')
+
+def add_underscore(string: str):
+  return string.replace(' ', '_')
+
 def pick_words(theme: str, count: int, allow_multiword=True, already_used=[]) -> List[str]:
   """
   Return a list of 'count' randomly selected words for given theme 'theme'.
   If 'allow_multiword' is false, selections consisting of multiple words (e.g., space-separated or hyphen-separated) will not be included.
   """
-  word_bank = themes[theme]
+
+  if theme in themes:
+    word_bank = themes[theme]
+  else:
+    word_bank = try_formats(theme, TIMES_MORE_WORDS * count)
+  
+  # TODO: We are not guaranteed to have enough words after we do this, we have to pass
+  # allow_multiword in the other functions as well
   if not allow_multiword:
     word_bank = list(filter(ALPHABETIC.fullmatch, word_bank))
 
@@ -24,3 +92,10 @@ def pick_words(theme: str, count: int, allow_multiword=True, already_used=[]) ->
 
   words = random.sample(word_bank, count)
   return words
+
+if __name__ == "__main__":
+  while True:
+    theme = input("Please enter a theme\n")
+    count = int(input("and the number of words you want to get back\n"))
+    words = pick_words(theme, count)
+    print(words)
