11b3c05c30aa3fb98c2373e6b3910eb79d0da2ef
Andrei-Florin Sburlan <as8319@ic.ac.uk>
2021-12-01 15:22:46 +0000

    Remove legacy login system, migrate to Firebase login

--- activitygen/__init__.py
+++ activitygen/__init__.py
@@ -1,23 +1,15 @@
-from flask import Flask, session
-from flask_session import Session
+from flask import Flask
 from flask_cors import CORS
-import redis
 import os
 
 def create_app(test_config=None):
   """Application factory"""
   app = Flask(__name__)
+  print(__name__)
  
   # Use test config if supplied
   if test_config:
     app.config.from_mapping(test_config)
-
-  app.config["SESSION_TYPE"]  = "redis"
-  app.config["SESSION_REDIS"] = redis.from_url('redis://redis:6379')
-  app.config['SECRET_KEY'] = 'super secret key'   
-
-  sess = Session()
-  sess.init_app(app)
 
   origins = ["http://localhost:3000"]
   if "FRONT_END_URL" in os.environ:
@@ -26,7 +18,7 @@ def create_app(test_config=None):
   CORS(app, origins = origins, supports_credentials=True)
 
   # Register blueprints
-  from . import root, anagrams, fill_in_the_blanks, maze, sudoku, word_search, login, books, image_shuffle, nonogram
+  from . import root, anagrams, fill_in_the_blanks, maze, sudoku, word_search, books, image_shuffle, nonogram
   app.register_blueprint(root.bp)
   app.register_blueprint(anagrams.bp)
   app.register_blueprint(fill_in_the_blanks.bp)
@@ -34,7 +26,6 @@ def create_app(test_config=None):
   app.register_blueprint(nonogram.bp)
   app.register_blueprint(sudoku.bp)
   app.register_blueprint(word_search.bp)
-  app.register_blueprint(login.bp)
   app.register_blueprint(books.bp)
   app.register_blueprint(image_shuffle.bp)
 
