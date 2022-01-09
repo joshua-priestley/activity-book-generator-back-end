9f6b8229072dd347e5bbbeb660b60376d882fdf9
Tiberiu Bucur <tiberiu.bucur19@imperial.ac.uk>
2022-01-09 15:28:21 +0200

    Remove global variables
    Note: This should get rid of the problems arising when multiple requests are handled simultaneously for the word search

--- activitygen/word_search.py
+++ activitygen/word_search.py
@@ -2,9 +2,7 @@ from math import sqrt, floor, log2, pow
 from random import shuffle, randint
 import string
 
-import sys
-
-from flask import Blueprint, jsonify, render_template, request
+from flask import Blueprint, render_template, request
 
 MAX_GRID_LENGTH = 20
 MAX_ATTEMPTS = 100
@@ -96,8 +94,6 @@ def generate(words, hidden_message = None):
   print(*words, sep=", ")
   print()
 
-  global hidden_msg_length
-
   if hidden_message != None:
     hidden_words = hidden_message.split()
     hangman_words = ["" for _ in hidden_words]
@@ -115,13 +111,11 @@ def generate(words, hidden_message = None):
 
   min_length, total_cells = compute_constraints(words, hidden_message)
 
-  global l, grid_size
-  l = compute_grid_length(min_length, total_cells)
-  grid_size = l * l
-  place_words(words)
-  place_hidden_message(hidden_message)
-  add_random_letters()
-  # print_board()
+  lng = compute_grid_length(min_length, total_cells)
+  (cells, lng, word_positions, remaining) = place_words(words, hidden_msg_length, lng)
+  cells = place_hidden_message(hidden_message, hidden_msg_length, cells, lng, remaining)
+  cells = add_random_letters(cells, lng)
+  # print_board(cells, lng)
   if hidden_message != None:
     print("Complete the secret message in the space below:")
     print(hangman_text + '\n')
@@ -135,57 +129,56 @@ def generate(words, hidden_message = None):
 
 ALPHABET_SIZE = 26
 
-def add_random_letters():
-  global cells
-  for i in range(l):
-    for j in range(l):
+def add_random_letters(cells, lng):
+  for i in range(lng):
+    for j in range(lng):
       if cells[i][j] == '':
         cells[i][j] = chr(ord('a') + randint(0, ALPHABET_SIZE - 1))
+  return cells
 
-
-def print_board():
+def print_board(cells, lng):
   print("\n    ", end='')
-  for i in range(l):
+  for i in range(lng):
     print(" %c " % str(i), end='')
   print("\n")
 
-  for i in range(l):
+  for i in range(lng):
     print("{0}   ".format(i), end='')
-    for j in range(l):
+    for j in range(lng):
       print(" %c " % cells[i][j], end='')
 
     print()
   print()
 
 
-def place_hidden_message(msg):
+def place_hidden_message(msg, hidden_msg_length, cells, lng, remaining):
   if msg == None:
-    return
-
-  global cells
+    return cells
 
   letter_gap = remaining // hidden_msg_length - 1
   msg_len = len(msg)
   cnt = crt_gap = 0
-  for i in range(l):
-    for j in range(l):
+  for i in range(lng):
+    for j in range(lng):
       if cells[i][j] == '':
         if crt_gap == letter_gap:
           cells[i][j] = msg[cnt]
           cnt += 1
           crt_gap = 0
           if cnt == msg_len:
-            return
+            return cells
         else:
           crt_gap += 1
 
-def place_words(words):
-  global l, grid_size
+  print("This should never be reached!")
+  return cells
+
+def place_words(words, hidden_msg_length, lng):
+  grid_size = lng * lng
   while True:
     attempts = 0
     while attempts < MAX_ATTEMPTS:
-      global cells, remaining, word_positions
-      cells = [['' for _ in range(l)] for _ in range(l)]
+      cells = [['' for _ in range(lng)] for _ in range(lng)]
       remaining = grid_size
       word_positions = {}
       attempts += 1
@@ -193,21 +186,22 @@ def place_words(words):
       success = True
 
       for word in words:
-        if not try_place_word(word):
+        (succ_place, cells, word_positions, remaining) = try_place_word(word, grid_size, cells, lng, word_positions, remaining)
+        if not succ_place:
           # We failed to place the current word on the grid, start fresh
           success = False
           break
 
       # If we managed to find a solution we return it
       if success and remaining >= hidden_msg_length:
-        return cells
+        return (cells, lng, word_positions, remaining)
       # Otherwise we try again
     
     # If we could not fill this grid, we try a bigger one
-    l += 1
-    grid_size = l * l
+    lng += 1
+    grid_size = lng * lng
 
-def try_place_word(word):
+def try_place_word(word, grid_size, cells, lng, word_positions, remaining):
   rand_pos = randint(0, grid_size - 1)
   rand_dir = randint(0, len(directions) - 1)
 
@@ -216,32 +210,32 @@ def try_place_word(word):
 
     for j in range(grid_size):
       pos = (j + rand_pos) % grid_size
-      if try_position(word, pos, dir):
-        return True
+      (success_pos, cells, word_positions, remaining) = try_position(word, pos, dir, cells, lng, word_positions, remaining) 
+      if success_pos:
+        return (True, cells, word_positions, remaining)
 
-  return False
+  return (False, cells, word_positions, remaining)
 
 
-def try_position(word, pos, dir):
-  r = pos // l
-  c = pos % l
+def try_position(word, pos, dir, cells, lng, word_positions, remaining):
+  r = pos // lng
+  c = pos % lng
   
   # Check bounds
   endr = r + dir[0] * len(word)
   endc = c + dir[1] * len(word)
-  if endr < 0 or endr >= l or endc < 0 or endc >= l:
-    return False
+  if endr < 0 or endr >= lng or endc < 0 or endc >= lng:
+    return (False, cells, word_positions, remaining)
 
   rr = r
   cc = c
   # Check we can add the word without changing any letter already written
   for i in range(len(word)):
     if cells[rr][cc] != '' and cells[rr][cc] != word[i]:
-      return False
+      return (False, cells, word_positions, remaining)
     rr += dir[0]
     cc += dir[1]
 
-  global remaining, word_positions
   word_positions[word] = ((r, c), (rr - dir[0], cc-dir[1]))
 
   for i in range(len(word)):
@@ -251,7 +245,7 @@ def try_position(word, pos, dir):
     r += dir[0]
     c += dir[1]
   
-  return True
+  return (True, cells, word_positions, remaining)
 
 def generate_html(data):
   return render_template("word-search.html", words=data["words"], cells=data["cells"], 
